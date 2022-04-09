4fdfd4c02873c00c42c2d56bdf1db14394a6fc79
Levratti S.r.l <102354521+Levratti@users.noreply.github.com>
2022-04-09 22:24:40 +0200

    Update Chiusura_RCMI_GeCo.user.js

--- Chiusura_RCMI_GeCo.user.js
+++ Chiusura_RCMI_GeCo.user.js
@@ -2,7 +2,7 @@
 // @name         Chiusura RCMI GeCo
 // @namespace    https://github.com/Levratti/GeCoScript
 // @version      0.1
-// @description  Crea il template per la chiusura della pratica in BO 
+// @description  Crea il template per la chiusura della pratica in BO
 // @author       Ruslan Dzyuba(Trorker)
 // @match        https://geco.impresalevratti.it/admin/backend/pratica/*/change/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=impresalevratti.it
@@ -23,42 +23,53 @@
             return xmlHttp.responseText;
         }
 
-        var eneltel = document.querySelector("#fieldsets-container > fieldset:nth-child(1) > div.form-row.field-eneltel.field-tipo_istallazione_display > div.fieldBox.field-eneltel > div.readonly").textContent;
-        var contratto = document.querySelector("#fieldsets-container > fieldset:nth-child(1) > div.form-row.field-contratto > div > div").textContent;
-        var DataIntervento = document.querySelector("#id_data_esecuzione_0").value;
-        var Operatore = document.querySelector("#fieldsets-container > fieldset:nth-child(2) > div.form-row.field-operatore > div > div.readonly").textContent;
-        var CE1G = document.querySelector("#fieldsets-container > fieldset:nth-child(1) > div.form-row.field-reso_link > div > div > a").textContent;
-        var CE2G = document.querySelector("#fieldsets-container > fieldset:nth-child(3) > div.form-row.field-contatore_link > div > div.readonly > a").textContent;
+        try {
+            var checkClouse = document.querySelector("#fieldsets-container > fieldset:nth-child(2) > div.form-row.field-stato_esecuzione_display_html > div > div").textContent;
+            if (checkClouse != "Eseguita") {
+                alert("Errore\nVerificare se la pratica risulta eseguita.");
+                return
+            }
 
-        var linkO = "https://geco.impresalevratti.it/admin/users/user/?q=" + Operatore.replace(" ", "+");
-        //console.log();
-        var htmlObject = document.createElement('html');
-        htmlObject.innerHTML = httpGet(linkO);
-        var MatOperatore = htmlObject.querySelector("#result_list > tbody > tr > th > a").textContent;
+            var eneltel = document.querySelector("#fieldsets-container > fieldset:nth-child(1) > div.form-row.field-eneltel.field-tipo_istallazione_display > div.fieldBox.field-eneltel > div.readonly").textContent;
+            var contratto = document.querySelector("#fieldsets-container > fieldset:nth-child(1) > div.form-row.field-contratto > div > div").textContent;
+            var DataIntervento = document.querySelector("#id_data_esecuzione_0").value;
+            var Operatore = document.querySelector("#fieldsets-container > fieldset:nth-child(2) > div.form-row.field-operatore > div > div.readonly").textContent;
+            var CE1G = document.querySelector("#fieldsets-container > fieldset:nth-child(1) > div.form-row.field-reso_link > div > div > a").textContent;
+            var CE2G = document.querySelector("#fieldsets-container > fieldset:nth-child(3) > div.form-row.field-contatore_link > div > div.readonly > a").textContent;
 
-
-        var a = window.open('','','width=400,height=250');
-        a.document.open("text/html");
-        a.document.write('<html><head><title>');
-        a.document.write('EnelTel');
-        a.document.write('</title>');
-        a.document.write('</head><body">');
-        a.document.write("------------------------------------------------------------------<br>");
-        a.document.write('<b>Richiesta chiusura RCMI ENELTEL ' + eneltel + '</b><br>');
-        a.document.write('<b>Contratto: </b>' + contratto + '<br>');
-        a.document.write('<b>Data intervento: </b>' + DataIntervento + '<br>');
-        a.document.write('<b>Matricola operatore: </b>' + MatOperatore + '<br>');
-        a.document.write('<b>CEID da rimuovere/rimosso: </b>' + CE1G + '<br>');
-        a.document.write('<b>CEID installato: </b>' + CE2G + '<br>');
-        a.document.write('<b>Letture di rimozione:</b><br>');
-        a.document.write('<b>&nbsp;&nbsp;&nbsp;&nbsp;A1: </b><br>');
-        a.document.write('<b>&nbsp;&nbsp;&nbsp;&nbsp;A2: </b><br>');
-        a.document.write('<b>&nbsp;&nbsp;&nbsp;&nbsp;A3: </b><br>');
-        a.document.write("</body></html>");
+            var linkO = "https://geco.impresalevratti.it/admin/users/user/?q=" + Operatore.replace(" ", "+");
+            //console.log();
+            var htmlObject = document.createElement('html');
+            htmlObject.innerHTML = httpGet(linkO);
+            var MatOperatore = htmlObject.querySelector("#result_list > tbody > tr > th > a").textContent;
 
 
-        a.document.close(); // necessary for IE >= 10
-        a.focus(); // necessary for IE >= 10*/
+            var a = window.open('','','width=400,height=250');
+            a.document.open("text/html");
+            a.document.write('<html><head><title>');
+            a.document.write('EnelTel');
+            a.document.write('</title>');
+            a.document.write('</head><body">');
+            a.document.write("------------------------------------------------------------------<br>");
+            a.document.write('<b>Richiesta chiusura RCMI ENELTEL ' + eneltel + '</b><br>');
+            a.document.write('<b>Contratto: </b>' + contratto + '<br>');
+            a.document.write('<b>Data intervento: </b>' + DataIntervento + '<br>');
+            a.document.write('<b>Matricola operatore: </b>' + MatOperatore + '<br>');
+            a.document.write('<b>CEID da rimuovere/rimosso: </b>' + CE1G + '<br>');
+            a.document.write('<b>CEID installato: </b>' + CE2G + '<br>');
+            a.document.write('<b>Letture di rimozione:</b><br>');
+            a.document.write('<b>&nbsp;&nbsp;&nbsp;&nbsp;A1: </b><br>');
+            a.document.write('<b>&nbsp;&nbsp;&nbsp;&nbsp;A2: </b><br>');
+            a.document.write('<b>&nbsp;&nbsp;&nbsp;&nbsp;A3: </b><br>');
+            a.document.write("</body></html>");
+
+
+            a.document.close(); // necessary for IE >= 10
+            a.focus(); // necessary for IE >= 10*/
+
+        } catch {
+            alert("Errore");
+        }
     }
 
     var btn = document.createElement("li");
